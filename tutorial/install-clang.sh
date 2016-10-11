ad70d3a5e9482af354bece4e4624bcc57e77c8fc
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-10-10 22:45:30 -0700

    Update install-clang.sh

--- tutorial/install-clang.sh
+++ tutorial/install-clang.sh
@@ -4,4 +4,5 @@ cd TMP_CLANG
 git clone https://chromium.googlesource.com/chromium/src/tools/clang
 cd ..
 TMP_CLANG/clang/scripts/update.py
-
+sudo cp -rf  third_party/llvm-build/Release+Asserts/lib/* /usr/local/lib/
+sudo cp -rf  third_party/llvm-build/Release+Asserts/bin/* /usr/local/bin
