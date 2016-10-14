5f435763ce0f9d3b8ee245582f0c6a94c4881d36
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-10-14 16:31:13 -0700

    add a proper test file for the boringssl benchmark

--- boringssl-2016-02-12/test-libfuzzer.sh
+++ boringssl-2016-02-12/test-libfuzzer.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Copyright 2016 Google Inc. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
+. $(dirname $0)/../common.sh
 
 # Note: this target contains unbalanced malloc/free (malloc is called
 # in one invocation, free is called in another invocation).
 # and so libFuzzer's -detect_leaks should be disabled for better speed.
+export ASAN_OPTIONS=detect_leaks=0:quarantine_size_mb=50
 
-echo "Not implemented yet, the bug takes several hours of fuzzing to find"
+set -x
+rm -rf $CORPUS
+mkdir $CORPUS
+rm -f *.log
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -artifact_prefix=$CORPUS/ -use_value_profile=1 -jobs=$JOBS -workers=$JOBS $CORPUS BUILD/fuzz/privkey_corpus
+grep "AddressSanitizer: heap-use-after-free" fuzz-0.log || exit 1
