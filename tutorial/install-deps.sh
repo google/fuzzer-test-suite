0218a7180b7cbaa5180d1fb6fe85cc0d85d96c48
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-10-14 21:18:49 -0700

    update openssl-1.0.2d deps

--- tutorial/install-deps.sh
+++ tutorial/install-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get --yes install subversion screen gcc g++ cmake ninja-build golang autoconf libtool apache2 python-dev pkg-config zlib1g-dev
+sudo apt-get --yes install subversion screen gcc g++ cmake ninja-build golang autoconf libtool apache2 python-dev pkg-config zlib1g-dev libgcrypt11-dev
