6a91bc221d2eac676ddb4e79670cfca285b0ebd5
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2016-10-25 16:08:22 -0700

    extend the woff test to also find a leak

--- woff2-2016-05-06/test-libfuzzer.sh
+++ woff2-2016-05-06/test-libfuzzer.sh
@@ -3,7 +3,15 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 . $(dirname $0)/../common.sh
 set -x
+
+# Find the buffer overflow (or OOM) with a seed corpus.
 rm -rf $CORPUS
 mkdir $CORPUS
-[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -artifact_prefix=$CORPUS/ -max_total_time=1800 -jobs=$JOBS -workers=$JOBS $CORPUS SEED_CORPUS
-grep "AddressSanitizer: heap-buffer-overflow" fuzz-0.log
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -use_cmp=1 -artifact_prefix=$CORPUS/ -max_total_time=1800 -jobs=$JOBS -workers=$JOBS $CORPUS SEED_CORPUS
+grep "AddressSanitizer: heap-buffer-overflow\|ERROR: libFuzzer: out-of-memory" fuzz-0.log  || exit 1
+
+# Find OOM bug with an empty seed corpus.
+rm -rf $CORPUS-1
+mkdir $CORPUS-1
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -use_cmp=1 -artifact_prefix=$CORPUS-1/ -max_total_time=600 -jobs=$JOBS -workers=$JOBS $CORPUS-1
+grep "ERROR: libFuzzer: out-of-memory" fuzz-0.log || exit 1
