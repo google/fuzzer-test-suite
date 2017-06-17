10f03d01c353bf601c1e0ef3b055f7be3df64c95
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2017-06-16 19:45:29 -0700

    use more iterations for re2

--- re2-2014-12-09/test-libfuzzer.sh
+++ re2-2014-12-09/test-libfuzzer.sh
@@ -6,10 +6,10 @@ set -x
 rm -rf $CORPUS
 mkdir $CORPUS
 rm -f *.log
-[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -artifact_prefix=$CORPUS/ -exit_on_src_pos=re2/dfa.cc:474 -exit_on_src_pos=re2/dfa.cc:474  -runs=1000000 -jobs=$JOBS -workers=$JOBS $CORPUS
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -artifact_prefix=$CORPUS/ -exit_on_src_pos=re2/dfa.cc:474 -exit_on_src_pos=re2/dfa.cc:474  -runs=10000000 -jobs=$JOBS -workers=$JOBS $CORPUS
 grep "INFO: found line matching 're2/dfa.cc:474', exiting." fuzz-0.log || exit 1
 
-# Also test mergind here
+# Also test merging here
 rm -rf $CORPUS-2
 mkdir $CORPUS-2
 [ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE $CORPUS-2 $CORPUS -merge=1 2> log
