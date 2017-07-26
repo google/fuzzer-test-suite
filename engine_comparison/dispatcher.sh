0ad297573c53cc143473c4c3be3b0f60a6050cb6
David Noursi <dcalifornia@google.com>
2017-07-26 13:53:32 -0700

    Improve dispatcher directories; make WORK w docker

--- engine_comparison/dispatcher.sh
+++ engine_comparison/dispatcher.sh
@@ -10,48 +10,54 @@ build_engine() {
   # All of this code will probably be in a separate script
 
   FENGINE_CONFIG=$1
-  . $FENGINE_CONFIG
+
   echo "Building $FENGINE_CONFIG"
-  # export ENGINE_DIR=${ENGINE_DIR}
+  export ENGINE_DIR=$WORK/fuzz-engines/${FENGINE_CONFIG}
+  rm -rf $ENGINE_DIR
+  mkdir $ENGINE_DIR
+
+  # Fuzzing activities go here
+
 }
 
 build_benchmark_using() {
   BENCHMARK=$1
   FENGINE_CONFIG=$2
 
-  # Write a new script here, probably
-  #
-  . $FENGINE_CONFIG
-
-  BUILDING_DIR=BUILD-${BENCHMARK}-WITH-${FENGINE_CONFIG}
+  echo "Filling $BUILDING_DIR"
+  BUILDING_DIR=$WORK/BUILD-${BENCHMARK}-WITH-${FENGINE_CONFIG}
   rm -rf $BUILDING_DIR
   mkdir $BUILDING_DIR
-  cd $BUILDING_DIR
-  echo "Filling $BUILDING_DIR"
 
-  # [[ ! -e ~/FTS/$1/build.sh ]] && echo "cant build" && exit 1
-  ~/FTS/$BENCHMARK/build.sh $FUZZING_ENGINE
+  # Do fuzzing things here
+  # [[ ! -e ~/FTS/$BENCHMARK/build.sh ]] && echo "cant build" && exit 1
+  # $WORK/FTS/$BENCHMARK/build.sh $FUZZING_ENGINE
 
-  # export SEND_DIR=SEND-${THIS_BENCHMARK}
+  export SEND_DIR=$WORK/SEND-${BENCHMARK}-WITH-${FENGINE_CONFIG}
+  rm -rf $SEND_DIR
+  mkdir $SEND_DIR
+  # Then construct the directory to send here, e.g.
   # cp ${BENCHMARK}-${FUZZING_ENGINE} $SEND_DIR
+  # copy seeds, afl-fuzz, etc
 
-  # copy seeds, afl-fuzz
+  rm -rf $BUILDING_DIR
 }
 
 BASE_INSTANCE_NAME="FTS-RUNNER"
 
-for FENGINE_CONFIG in $(find ./fengine-configs); do
+for FENGINE_CONFIG in $(find $WORK/fengine-configs); do
   build_engine $FENGINE_CONFIG
   for BENCHMARK in $ALL_BENCHMARKS; do
-    THIS_BENCHMARK=${BENCHMARK}-${FENGINE_CONFIG}
     # n.b. this requires each config file to have a different name
     build_benchmark_using $BENCHMARK $FENGINE_CONFIG
+
+    THIS_BENCHMARK=${BENCHMARK}-WITH-${FENGINE_CONFIG} # Just for convenience
 
     INSTANCE_NAME=${BASE_INSTANCE_NAME}-${THIS_BENCHMARK}
     gcloud compute instances create $INSTANCE_NAME
 
     gcloud compute ssh $INSTANCE_NAME --command="mkdir /input"
-    gcloud compute scp --recurse ./RUN-${THIS_BENCHMARK}/ ${INSTANCE_NAME}:/input/RUN-${THIS_BENCHMARK}
+    gcloud compute scp --recurse $WORK/SEND-${THIS_BENCHMARK}/ ${INSTANCE_NAME}:/input
     # probably want to scp SEND-$THIS_BENCHMARK instead
 
     RUNNER_COMMAND="docker build -f /RUN-${THIS_BENCHMARK} --build-arg run-script=runner.sh /input"
@@ -59,6 +65,7 @@ for FENGINE_CONFIG in $(find ./fengine-configs); do
   done
 done
 
+# rm -rf $WORK/SEND*
 
 ######
 # for time in 30s 1m 5m 10m 30m; do
