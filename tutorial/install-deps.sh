64bb0b0cb55a93dfbc83b582a0def3f8f0265c57
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2017-08-03 11:54:46 -0700

    Update install-deps.sh

--- tutorial/install-deps.sh
+++ tutorial/install-deps.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get --yes install subversion screen gcc g++ cmake ninja-build golang autoconf libtool apache2 python-dev pkg-config zlib1g-dev libgcrypt11-dev libgss-dev libssl-dev libxml2-dev
