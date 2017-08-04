333ac06db2db5eb4854026b73aa6a6c44968f04f
David Noursi <dcalifornia@google.com>
2017-08-04 11:29:00 -0700

    Fix AFL/libfuzzer inter dependency

--- engine-comparison/dispatcher.sh
+++ engine-comparison/dispatcher.sh
@@ -28,6 +28,10 @@ build_engine() {
   if [[ $FUZZING_ENGINE == "afl" ]]; then
 
     # [[ ! -d $LIBFUZZER_SRC ]] && echo "Can't do AFL before libfuzzer" && break
+    if [[ ! -d $LIBFUZZER_SRC ]]; then
+      mkdir -p ${LIBFUZZER_SRC}/afl
+      svn co http://llvm.org/svn/llvm-project/llvm/trunk/lib/Fuzzer/afl ${LIBFUZZER_SRC}/afl
+     fi
     echo "Making a version of afl"
     cd $WORK/fengine-builds
     wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
