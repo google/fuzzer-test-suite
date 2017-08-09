7322ad7056f864a6594ef091985ffc16b2ca5e53
David Noursi <dcalifornia@google.com>
2017-08-09 09:45:24 -0700

    Message grammar

--- common.sh
+++ common.sh
@@ -9,8 +9,8 @@
 FUZZING_ENGINE=${FUZZING_ENGINE:-"libfuzzer"}
 POSSIBLE_FUZZING_ENGINE="libfuzzer afl coverage fsanitize_fuzzer"
 !(echo "$POSSIBLE_FUZZING_ENGINE" | grep -w "$FUZZING_ENGINE" > /dev/null) && \
-  echo "USAGE: Error: If defined, FUZZING_ENGINE should be among $POSSIBLE_FUZZING_ENGINE;\
-  on the contrary, it was defined as $FUZZING_ENGINE" && exit 1
+  echo "USAGE: Error: If defined, FUZZING_ENGINE should be one of the following:
+  $POSSIBLE_FUZZING_ENGINE. However, it was defined as $FUZZING_ENGINE" && exit 1
 
 SCRIPT_DIR=$(dirname $0)
 EXECUTABLE_NAME_BASE=$(basename $SCRIPT_DIR)-${FUZZING_ENGINE}
