6245972fd7a87d2a013251e7057b6732931dfe53
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2017-08-11 12:36:04 -0700

    use -fsanitize=address,fuzzer-no-link for compiling and -fsanitize=fuzzer for linking; minor edit in fuzz_me.cc

--- tutorial/fuzz_me.cc
+++ tutorial/fuzz_me.cc
@@ -1,8 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
-bool FuzzMe(const uint8_t *Data,
-            size_t DataSize) {
+bool FuzzMe(const uint8_t *Data, size_t DataSize) {
   return DataSize >= 3 &&
       Data[0] == 'F' &&
       Data[1] == 'U' &&
