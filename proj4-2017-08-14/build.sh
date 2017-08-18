9ce3a8cc01834ca63d9f6c9e5b15ca5831df20c2
David Noursi <dcalifornia@google.com>
2017-08-18 17:33:04 +0000

    WIP proj4 benchmark

---
+++ proj4-2017-08-14/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright 2017 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+. $(dirname $0)/../common.sh
+
+build_lib() {
+  rm -rf BUILD
+  cp -rf SRC BUILD
+  (cd BUILD && ./autogen.sh &&  ./configure  &&  make clean  && make -j $JOBS )
+}
+
+get_git_revision https://github.com/OSGeo/proj.4.git d00501750b210a73f9fb107ac97a683d4e3d8e7a SRC
+build_lib
+build_fuzzer
+
+mkdir seeds
+cp -r BUILD/nad/* seeds
+
+$CXX $CXXFLAGS -std=c++11 -I BUILD/src  BUILD/test/fuzzers/standard_fuzzer.cpp  BUILD/src/.libs/libproj.a $LIB_FUZZING_ENGINE -lpthread -o $EXECUTABLE_NAME_BASE
+
+#-DSTANDALONE
+
+
+
