8de58ed8998aa470c78d8648607bb4b1944f2f2c
David Noursi <dcalifornia@google.com>
2017-08-18 11:19:38 -0700

    Conditionally create seeds/

--- proj4-2017-08-14/build.sh
+++ proj4-2017-08-14/build.sh
@@ -13,7 +13,9 @@ get_git_revision https://github.com/OSGeo/proj.4.git d00501750b210a73f9fb107ac97
 build_lib
 build_fuzzer
 
-mkdir seeds
-cp -r BUILD/nad/* seeds
+if [[ ! -d seeds ]]; then
+  mkdir seeds
+  cp BUILD/nad/* seeds
+fi
 
 $CXX $CXXFLAGS -std=c++11 -I BUILD/src BUILD/test/fuzzers/standard_fuzzer.cpp BUILD/src/.libs/libproj.a $LIB_FUZZING_ENGINE -o $EXECUTABLE_NAME_BASE
