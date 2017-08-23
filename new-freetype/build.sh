f84b1b6f833e4e0ada362412c79dea3e589ecf1a
David Noursi <dcalifornia@google.com>
2017-08-23 14:12:15 -0700

    Test-libfuzzer for freetype

--- new-freetype/build.sh
+++ new-freetype/build.sh
@@ -12,10 +12,15 @@ build_lib() {
 get_git_tag git://git.sv.nongnu.org/freetype/freetype2.git VER-2-8 SRC
 build_lib
 build_fuzzer
+
+if [[ ! -d seeds ]]; then
+  mkdir seeds
+  (cd seeds && wget https://github.com/adobe-fonts/adobe-variable-font-prototype/releases/download/1.001/AdobeVFPrototype.otf)
+  git clone https://github.com/unicode-org/text-rendering-tests.git
+  cp text-rendering-tests/fonts/* seeds
+  rm -fr text-rendering-tests
+fi
+
 set -x
-
-$CXX $CXXFLAGS -std=c++11   -I BUILD/include -I BUILD/ BUILD/src/tools/ftfuzzer/ftfuzzer.cc BUILD/objs/.libs/libfreetype.a  $LIB_FUZZING_ENGINE -larchive -lz -o $EXECUTABLE_NAME_BASE
-
-
-
+$CXX $CXXFLAGS -std=c++11 -I BUILD/include -I BUILD/ BUILD/src/tools/ftfuzzer/ftfuzzer.cc BUILD/objs/.libs/libfreetype.a  $LIB_FUZZING_ENGINE -larchive -lz -o $EXECUTABLE_NAME_BASE
 
