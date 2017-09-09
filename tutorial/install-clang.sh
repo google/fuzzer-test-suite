e15b798d65253b02187cd0aea72b0590955bfbeb
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2017-09-08 17:26:23 -0700

    use clang 5.0.0

--- tutorial/install-clang.sh
+++ tutorial/install-clang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CLANG_VERSION=4.0.1
-CLANG_DIR=clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-debian8
-curl http://releases.llvm.org/$CLANG_VERSION/$CLANG_DIR.tar.xz | tar xfJ - 
+CLANG_VERSION=5.0.0
+CLANG_DIR=clang+llvm-$CLANG_VERSION-linux-x86_64-ubuntu16.04
+curl http://releases.llvm.org/$CLANG_VERSION/$CLANG_DIR.tar.xz | tar xfJ -
 sudo rm -rf /usr/local/bin/clang* /usr/local/lib/clang
 sudo cp -rf  $CLANG_DIR/bin/*  /usr/local/bin
 sudo cp -rf  $CLANG_DIR/lib/clang  /usr/local/lib
