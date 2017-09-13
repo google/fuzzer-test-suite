fa6ee1dd73c0d70d5815c15d645d4c61b4f39c31
David Noursi <dcalifornia@google.com>
2017-09-13 13:33:53 -0700

    Doc gsutil report gen

--- engine-comparison/report-gen/gsutil-report-gen.sh
+++ engine-comparison/report-gen/gsutil-report-gen.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+# Copyright 2016 Google Inc. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
 
-# Not calling common scripts, because they don't allow being called in directory
+# This script generates reports from the local machine. To produce reports,
+# call this script from any directory. The folder "reports" will be cloned and manipulated,
+# then the graphs will be hosted as a webpage on GCP Storage.
+
+# Not calling common scripts, because they don't allow being called in
+# directory:
 #. $(dirname $0)/../../common.sh
 #. ${SCRIPT_DIR}/../common-harness.sh
+#. $(dirname $0)/../common-harness.sh
+
 # This is all that we need from them:
 GSUTIL_BUCKET="gs://fuzzer-test-suite"
 
