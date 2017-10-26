dbd3785f97fb0b8d164197e7c8550cf817a704b3
Matt Morehouse <mascasa@google.com>
2017-10-26 16:51:58 -0700

    Add latest tag for pulling from GCR.
    Fixes transient failures when pulling base image.

--- engine-comparison/startup-dispatcher.sh
+++ engine-comparison/startup-dispatcher.sh
@@ -5,7 +5,7 @@
 # Script to run on creation of the dispatcher VM.
 # Configures dispatcher container and runs the dispatcher script.
 
-sudo gcloud docker -- pull gcr.io/fuzzer-test-suite/gcloud-clang-deps
+sudo gcloud docker -- pull gcr.io/fuzzer-test-suite/gcloud-clang-deps:latest
 find ~/input/FTS/ -name "*.sh" -exec chmod 750 {} \;
 sudo docker build -t base-image \
   -f ~/input/FTS/engine-comparison/Dockerfile-dispatcher ~/input
