93284f7d9feb49a2c4b488717fe78f913afaf06d
Matt Morehouse <mascasa@google.com>
2017-11-01 09:57:33 -0700

    Stop rsyncing seconds files for each cycle.
    The purpose of the files was to record the exact number of seconds
    elapsed since the benchmark started.  Due to David's previous
    improvements to the snapshotting and rsyncing process, snapshots have
    been occurring at exactly 20 second intervals without fail.  So we can
    now confidently infer the seconds elapsed with just the snapshot number.

--- engine-comparison/runner.sh
+++ engine-comparison/runner.sh
@@ -43,7 +43,6 @@ conduct_experiment() {
     # Snapshot
     cp -r corpus corpus-copy
 
-    echo "VM_SECONDS=${SECONDS}" > results/seconds-${cycle}
     if diff <(ls corpus-copy) <(ls last-corpus); then
       # Corpus is unchanged; avoid rsyncing it.
       echo "${cycle}" >> results/unchanged-cycles
@@ -56,7 +55,6 @@ conduct_experiment() {
     # Done with snapshot
     rm -r last-corpus
     mv corpus-copy last-corpus
-    rm "results/seconds-${cycle}"
     rm "corpus-archives/corpus-archive-${cycle}.tar.gz"
 
     cycle=$((cycle + 1))
