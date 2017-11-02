a66e553d3cf86ff9bc7f23c7d1cd3ebcb5920fb6
Matt Morehouse <mascasa@google.com>
2017-11-02 09:26:00 -0700

    Only sync reproducers from the current trial.

--- engine-comparison/runner.sh
+++ engine-comparison/runner.sh
@@ -68,7 +68,7 @@ conduct_experiment() {
   done
 
   # Sync final fuzz log
-  cp fuzz-0.log crash* leak* timeout* oom* results/
+  mv fuzz-0.log crash* leak* timeout* oom* results/
   rsync_no_delete results "${sync_dir}/results"
 }
 
