3c8257f55dba6c6fbdc08bf0be0d6b67befbb75f
Matt Morehouse <mascasa@google.com>
2017-11-30 14:39:45 -0800

    Remove config directory.
    User must now specify experiment config file on the command line.
    Generated keys and other configs are created in the CWD, with the
    service account key still reused when present.

--- engine-comparison/begin-experiment.sh
+++ engine-comparison/begin-experiment.sh
@@ -10,43 +10,48 @@
 
 set -eu
 
-if [[ -z $1 || -z $2 ]]; then
-  echo "Usage: $0 benchmark1[,benchmark2,...] fuzz-config1 [fuzz-config2 ...]"
+if [[ -z ${1+x} || -z ${2+x} || -z ${3+x} ]]; then
+  echo "Usage: $0 benchmark1[,benchmark2,...] experiment-config fuzz-config1 [fuzz-config2 ...]"
   exit 1
 fi
 
-# Write bmarks to file for dispatcher to read.
-readonly CONFIG_DIR="${SCRIPT_DIR}/config"
+# Set up experiment configuration directory for dispatcher to use.
+readonly CONFIG_DIR="config"
+rm -rf "${CONFIG_DIR}"
+mkdir "${CONFIG_DIR}"
+cp "$2" "${CONFIG_DIR}/parameters.cfg"
 . "${CONFIG_DIR}/parameters.cfg"
+
+# Write bmarks to file for dispatcher to read.
 echo "BMARKS=$1" > "${CONFIG_DIR}/bmarks.cfg"
 
 # Copy fuzzing configs to single directory for sending to dispatcher.
-readonly FENGINE_CONFIG_DIR="${CONFIG_DIR}/fengine-configs"
-declare -ar FENGINE_CONFIGS=( "${@:2}" )
+readonly FENGINE_CONFIG_DIR="fengine-configs"
+declare -ar FENGINE_CONFIGS=( "${@:3}" )
 rm -rf "${FENGINE_CONFIG_DIR}"
 mkdir "${FENGINE_CONFIG_DIR}"
 for fengine_config in "${FENGINE_CONFIGS[@]}"; do
   cp "${fengine_config}" "${FENGINE_CONFIG_DIR}/"
 done
 
-# Pass service account auth key
-if [[ ! -e "${CONFIG_DIR}/autogen-PRIVATE-key.json" ]]; then
-  gcloud iam service-accounts keys create \
-    "${CONFIG_DIR}/autogen-PRIVATE-key.json" \
+# Create or reuse service account auth key.
+if [[ ! -e "./autogen-PRIVATE-key.json" ]]; then
+  gcloud iam service-accounts keys create autogen-PRIVATE-key.json \
     --iam-account="${SERVICE_ACCOUNT}" --key-file-type=json
 fi
+cp autogen-PRIVATE-key.json "${CONFIG_DIR}/"
 
-# -m parallelizes operation; -r sets recursion, -d syncs deletion of files
 gsutil -m rsync -rd "${FENGINE_CONFIG_DIR}" \
   "${GSUTIL_BUCKET}/${EXPERIMENT}/input/fengine-configs"
-rm -rf "${FENGINE_CONFIG_DIR}"
 
-# Send the entire local FTS repository to the dispatcher;
-# Local changes to any file will propagate
+# Send the entire local FTS repository to the dispatcher.
+# Local changes to any file will propagate.
 gsutil -m rsync -rd -x ".git/*" "$(dirname "${SCRIPT_DIR}")" \
   "${GSUTIL_BUCKET}/${EXPERIMENT}/input/FTS"
 
-#gsutil -m acl ch -r -u ${SERVICE_ACCOUNT}:O ${GSUTIL_BUCKET}
+# Send other configs.
+gsutil -m rsync -rd "${CONFIG_DIR}" \
+  "${GSUTIL_BUCKET}/${EXPERIMENT}/input/FTS/engine-comparison/config"
 
 # Set up dispatcher and run its startup script.
 readonly INSTANCE_NAME="dispatcher-${EXPERIMENT}"
