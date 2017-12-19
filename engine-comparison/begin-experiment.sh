f753559246d01c74a9cc363777fda5a6ad8a14ba
Matt Morehouse <mascasa@google.com>
2017-12-19 09:57:00 -0800

    Implement experiment configuration validation.
    Informs user of invalid configurations early rather than launching an
    experiment that will fail.

--- engine-comparison/begin-experiment.sh
+++ engine-comparison/begin-experiment.sh
@@ -22,6 +22,34 @@ mkdir "${CONFIG_DIR}"
 cp "$2" "${CONFIG_DIR}/parameters.cfg"
 . "${CONFIG_DIR}/parameters.cfg"
 
+# Validate experiment configuration
+if [[ -z ${EXPERIMENT+x} || -z ${RUNNERS+x} || -z ${JOBS+x} || \
+  -z ${MAX_RUNS+x} || -z ${MAX_TOTAL_TIME+x} ]]; then
+  echo "Error: experiment-config must define EXPERIMENT, RUNNERS, JOBS, MAX_RUNS, and MAX_TOTAL_TIME"
+  exit 1
+fi
+if ! echo "${EXPERIMENT}" | grep "^[a-z0-9-][a-z0-9-]*$" &> /dev/null; then
+  echo "Error: EXPERIMENT must match [a-z0-9-]+"
+  exit 1
+fi
+for numeric_param in ${RUNNERS} ${JOBS} ${MAX_RUNS} ${MAX_TOTAL_TIME}; do
+  numeric_regex="^-?[0-9]+$"
+  if [[ ! "${numeric_param}" =~ ${numeric_regex} ]]; then
+    echo "Error: RUNNERS, JOBS, MAX_RUNS, and MAX_TOTAL_TIME must be integers"
+    exit 1
+  fi
+done
+
+# Validate benchmark names
+readonly NUM_BAD_NAMES="$(comm -23 \
+  <(echo "$1" | tr "," "\n" | sort -u) \
+  <((ls "${SCRIPT_DIR}/.."; echo all; echo most; echo small) | sort -u) \
+  | wc -l)"
+if [[ "${NUM_BAD_NAMES}" -ne 0 ]]; then
+  echo "Error: ${NUM_BAD_NAMES} benchmark names are invalid"
+  exit 1
+fi
+
 # Write bmarks to file for dispatcher to read.
 echo "BMARKS=$1" > "${CONFIG_DIR}/bmarks.cfg"
 
@@ -30,7 +58,27 @@ readonly FENGINE_CONFIG_DIR="fengine-configs"
 declare -ar FENGINE_CONFIGS=( "${@:3}" )
 rm -rf "${FENGINE_CONFIG_DIR}"
 mkdir "${FENGINE_CONFIG_DIR}"
+readonly RESERVED_LENGTH="$( \
+  find "${SCRIPT_DIR}/.." -maxdepth 1 -mindepth 1 -printf "%f\n" \
+  | awk '{ print length }' \
+  | sort -rn \
+  | head -n1)"
+readonly MAX_EXP_FENGINE_LENGTH=$((61 - RESERVED_LENGTH))
 for fengine_config in "${FENGINE_CONFIGS[@]}"; do
+  fengine_name="$(basename "${fengine_config}")"
+  # Validate fengine_config name
+  if ! echo "${fengine_name}" | grep "^[a-z0-9-][a-z0-9-]*$" &> /dev/null
+  then
+    echo "Error: fuzz-configs must match [a-z0-9-]+"
+    exit 1
+  fi
+  # Ensure runner VM names won't be too long.
+  exp_fengine_length="$(echo "${EXPERIMENT}${fengine_name}" | wc -c)"
+  if [[ ${exp_fengine_length} -gt ${MAX_EXP_FENGINE_LENGTH} ]]; then
+    echo "Error: len(${EXPERIMENT}) + len(${fengine_name}) = ${exp_fengine_length}"
+    echo "       Sum must be less than ${MAX_EXP_FENGINE_LENGTH} due to VM naming restrictions"
+    exit 1
+  fi
   cp "${fengine_config}" "${FENGINE_CONFIG_DIR}/"
 done
 
