c9b261352deeef9262399ea4dcfcccb331d2f8b4
Matt Morehouse <mascasa@google.com>
2018-01-04 12:32:07 -0800

    Validate GCP configuration.
    Fail fast when GCP configuration is missing.

--- engine-comparison/begin-experiment.sh
+++ engine-comparison/begin-experiment.sh
@@ -24,8 +24,13 @@ cp "$2" "${CONFIG_DIR}/parameters.cfg"
 
 # Validate experiment configuration
 if [[ -z ${EXPERIMENT+x} || -z ${RUNNERS+x} || -z ${JOBS+x} || \
-  -z ${MAX_RUNS+x} || -z ${MAX_TOTAL_TIME+x} ]]; then
-  echo "Error: experiment-config must define EXPERIMENT, RUNNERS, JOBS, MAX_RUNS, and MAX_TOTAL_TIME"
+  -z ${MAX_RUNS+x} || -z ${MAX_TOTAL_TIME+x} || \
+  -z ${CLOUDSDK_COMPUTE_ZONE+x} || -z ${GSUTIL_BUCKET+x} || \
+  -z ${GSUTIL_WEB_BUCKET+x} || -z ${SERVICE_ACCOUNT+x} ]]; then
+  echo "Error: experiment-config must define the following parameters:"
+  echo "  EXPERIMENT, RUNNERS, JOBS, MAX_RUNS, MAX_TOTAL_TIME,"
+  echo "  CLOUDSDK_COMPUTE_ZONE, GSUTIL_BUCKET, GSUTIL_WEB_BUCKET,"
+  echo "  and SERVICE_ACCOUNT"
   exit 1
 fi
 if ! echo "${EXPERIMENT}" | grep "^[a-z0-9-][a-z0-9-]*$" &> /dev/null; then
@@ -36,6 +41,13 @@ for numeric_param in ${RUNNERS} ${JOBS} ${MAX_RUNS} ${MAX_TOTAL_TIME}; do
   numeric_regex="^-?[0-9]+$"
   if [[ ! "${numeric_param}" =~ ${numeric_regex} ]]; then
     echo "Error: RUNNERS, JOBS, MAX_RUNS, and MAX_TOTAL_TIME must be integers"
+    exit 1
+  fi
+done
+for bucket_param in ${GSUTIL_BUCKET} ${GSUTIL_WEB_BUCKET}; do
+  bucket_regex="^gs://"
+  if [[ ! "${bucket_param}" =~ ${bucket_regex} ]]; then
+    echo "Error: GSUTIL_BUCKET and GSUTIL_WEB_BUCKET must start with gs://"
     exit 1
   fi
 done
