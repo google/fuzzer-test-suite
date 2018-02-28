3a6fd8a971a2858b6f20a01323b4e4c2784acb8e
Matt Morehouse <mascasa@google.com>
2018-02-28 15:37:29 -0800

    Allow custom builds on openthread, wpantund, libpng.

--- libpng-1.2.56/build.sh
+++ libpng-1.2.56/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Copyright 2016 Google Inc. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
+. $(dirname $0)/../custom-build.sh $1 $2
 . $(dirname $0)/../common.sh
 
 [ ! -e libpng-1.2.56.tar.gz ] && wget https://downloads.sourceforge.net/project/libpng/libpng12/older-releases/1.2.56/libpng-1.2.56.tar.gz
@@ -12,7 +13,11 @@ build_lib() {
   (cd BUILD && ./configure &&  make -j)
 }
 
-build_lib
-build_fuzzer
+build_lib || exit 1
+build_fuzzer || exit 1
+if [[ $FUZZING_ENGINE == "hooks" ]]; then
+  # Link ASan runtime so we can hook memcmp et al.
+  LIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE -fsanitize=address"
+fi
 set -x
 $CXX $CXXFLAGS -std=c++11 $SCRIPT_DIR/target.cc BUILD/.libs/libpng12.a $LIB_FUZZING_ENGINE -I BUILD/ -I BUILD -lz -o $EXECUTABLE_NAME_BASE
