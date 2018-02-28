16ac96480964be9bbc5d212876ffb3f2b26466a9
Matt Morehouse <mascasa@google.com>
2018-02-28 15:14:51 -0800

    Create simple interface for custom builds.
    By including custom-build.sh in a build.sh script, we can now pass
    arguments to the build like so:
      ${BENCHMARK}/build.sh [asan|ubsan|hooks] [hooks-file]
    
    asan and ubsan modes build with ASan and UBsan, respectively.  hooks
    mode builds with the user-defined hooks in hooks-file.
    tutorial/example-hooks.cc provides an example hooks file that simply
    dumps coverage information.

---
+++ custom-build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+MODE=$1
+HOOKS_FILE=$2
+
+if [[ -n "${MODE}" ]]; then
+  case "${MODE}" in
+    asan)
+      export FUZZING_ENGINE=fsanitize_fuzzer
+      ;;
+    ubsan)
+      export FUZZING_ENGINE=fsanitize_fuzzer
+      export CFLAGS="-O2 -fno-omit-frame-pointer -g -fsanitize=undefined,fuzzer-no-link"
+      export CXXFLAGS="${CFLAGS}"
+      ;;
+    hooks)
+      if [[ ! -f "${HOOKS_FILE}" ]]; then
+        echo "Error: Missing hooks file"
+        exit 1
+      fi
+      export FUZZING_ENGINE=hooks
+      export CFLAGS="-O0 -fsanitize-coverage=trace-pc-guard,trace-cmp,trace-gep,trace-div"
+      export CXXFLAGS="${CFLAGS}"
+      export HOOKS_FILE
+      ;;
+    *)
+      echo "Error: Unknown mode: ${MODE}"
+      exit 1
+      ;;
+  esac
+fi
+
