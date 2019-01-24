248a4839b1cbe5905466e86de655a25897ee40cc
Kostya Serebryany <kcc@google.com>
2019-01-23 17:28:14 -0800

    [libpng] plug the png_mutator.h into the fuzzer via the compiler flag -include, instead of actually including it

--- libpng-1.2.56/target.cc
+++ libpng-1.2.56/target.cc
@@ -129,8 +129,3 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   }
   return 0;
 }
-
-// Include png_mutator for structure-aware-fuzzing (aka custom mutator).
-// To actually enable it need to also define
-// PNG_MUTATOR_DEFINE_LIBFUZZER_CUSTOM_MUTATOR (e.g. in the build command).
-#include "png_mutator.h"
