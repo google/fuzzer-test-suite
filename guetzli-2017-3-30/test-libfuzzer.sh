333722fb4ff470dd60ada2b6253e6d5c10978ec1
Adrian Herrera <adrian.herrera02@gmail.com>
2019-10-21 10:20:40 +1100

    Added missing test-libfuzzer.sh scripts

--- guetzli-2017-3-30/test-libfuzzer.sh
+++ guetzli-2017-3-30/test-libfuzzer.sh
@@ -5,7 +5,7 @@
 set -x
 rm -rf $CORPUS
 
-cp -r $SCRIPT_DIR/seeds $CORPUS
+mkdir $CORPUS
 
-[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -max_len=180 -use_value_profile=1 -close_fd_mask=3 -dict=$SCRIPT_DIR/jpeg.dict -artifact_prefix=$CORPUS/ -jobs=$JOBS -workers=$JOBS $LIBFUZZER_FLAGS $CORPUS
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -max_len=180 -use_value_profile=1 -close_fd_mask=3 -dict=$SCRIPT_DIR/jpeg.dict -artifact_prefix=$CORPUS/ -jobs=$JOBS -workers=$JOBS $LIBFUZZER_FLAGS $CORPUS $SCRIPT_DIR/seeds
 grep "ERROR: libFuzzer: deadly signal" fuzz-0.log || exit 1
